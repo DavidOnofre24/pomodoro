2bb0f5aa89b4b5e1f526329983cefd7d0376f2a8
David Onofre <davidonofre1010@gmail.com>
2024-04-17 18:12:00 -0500

    init pomodoro

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+          accentColor: Colors.greenAccent,
+          brightness: Brightness.dark,
+          backgroundColor: Colors.black.withOpacity(0.1),
+        ),
+      );
+}
---
+++ lib/presentacion/screens/screen.dart
@@ -0,0 +1,1 @@
+export 'package:pomodoro/presentacion/screens/home_screen.dart';
